a4f317e56b782811a1b2b164414471f4b691f397
Luc Boudreau <lucboudreau@gmail.com>
2019-11-15 19:31:54 -0500

    [BAD-1906] First try at a google dataproc shim for v1.25. HDFS still broken. Some weird stuff with version numbers left to figure out. Hive works.

---
+++ shims/dataproc125/driver/src/main/java/org/pentaho/hadoop/shim/HadoopShim.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.hadoop.shim;
+
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.hadoop.hbase.CompatibilityFactory;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.ipc.RpcServer;
+import org.apache.hadoop.hbase.mapreduce.JobUtil;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.metrics.Snapshot;
+import org.apache.hadoop.hbase.metrics.impl.FastLongHistogram;
+import org.apache.hadoop.hbase.zookeeper.ZKWatcher;
+import org.apache.hbase.thirdparty.com.google.common.collect.Lists;
+import org.apache.hbase.thirdparty.com.google.protobuf.UnsafeByteOperations;
+import org.apache.hbase.thirdparty.io.netty.channel.Channel;
+import org.apache.htrace.core.Tracer;
+import org.apache.zookeeper.ZooKeeper;
+import org.pentaho.hadoop.shim.api.internal.Configuration;
+import org.pentaho.hadoop.shim.api.internal.mapred.RunningJob;
+import org.pentaho.hadoop.shim.common.ConfigurationProxyV2;
+import org.pentaho.hadoop.shim.common.HadoopShimImpl;
+
+import com.codahale.metrics.MetricRegistry;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.core.Versioned;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.Message;
+
+public class HadoopShim extends HadoopShimImpl {
+
+  public final String GS_STAGE_PATH = "DATAPROC.GS_STAGE_PATH";
+  public final String OUTPUT_BQFF = "DATAPROC.OUTPUT_BQFF";
+
+  public HadoopShim() {
+    super();
+  }
+
+  @Override
+  public Class[] getHbaseDependencyClasses() {
+    return new Class[] {
+      HConstants.class, org.apache.hadoop.hbase.protobuf.generated.ClientProtos.class,
+      org.apache.hadoop.hbase.shaded.protobuf.generated.ClientProtos.class, Put.class,
+      RpcServer.class, CompatibilityFactory.class, JobUtil.class, TableMapper.class, FastLongHistogram.class,
+      Snapshot.class, ZooKeeper.class, Channel.class, Message.class, UnsafeByteOperations.class, Lists.class,
+      Tracer.class, MetricRegistry.class, ArrayUtils.class, ObjectMapper.class, Versioned.class, JsonView.class,
+      ZKWatcher.class
+    };
+  }
+
+  @Override
+  public RunningJob submitJob( Configuration c ) throws IOException {
+    // TODO Auto-generated method stub
+    return super.submitJob( c );
+  }
+
+  @SuppressWarnings( "unchecked" )
+  @Override
+  public org.pentaho.hadoop.shim.api.internal.Configuration createConfiguration( String namedClusterConfigId ) {
+    ConfigurationProxyV2 conf = (ConfigurationProxyV2) super.createConfiguration( namedClusterConfigId );
+    conf.getJob().getConfiguration().setRestrictSystemProperties( false );
+
+//    // Dataproc config
+//
+//    try {
+//
+//      final Path outputPath = FileOutputFormat.getOutputPath( conf.getJob() );
+//      final Path[] inputPath = FileInputFormat.getInputPaths( conf.getJobConf() );
+//
+//      BigQueryConfiguration.configureBigQueryInput(
+//        conf.getJob().getConfiguration(),
+//        inputPath[0].toUri().getPath());
+//
+//      // Define an output schema in JSON form
+//      // TODO Make the names of the two fields dynamic based on the
+//      // hadoop output step in the KTRs.
+//      List<TableFieldSchema> outputTableFieldSchema = new ArrayList<TableFieldSchema>();
+//      outputTableFieldSchema.add(new TableFieldSchema().setName("Key").setType("STRING"));
+//      outputTableFieldSchema.add(new TableFieldSchema().setName("Value").setType("STRING"));
+//      TableSchema outputSchema = new TableSchema().setFields(outputTableFieldSchema);
+//
+//      BigQueryOutputConfiguration.configure(
+//        conf.getJob().getConfiguration(),
+//        outputPath.toUri().getPath(),
+//        outputSchema.toString(),
+//        // TODO need a user property with key DATAPROC.GS_STAGE_PATH
+//        // to point to the staging path in GCS. Review this.
+//        // example: 'gs://bucket/job'
+//        conf.get( GS_STAGE_PATH, "gs://bucket/job" ),
+//        // TODO need a user property with key DATAPROC.OUTPUT_BQFF
+//        // to point to one of 'CSV', 'NEWLINE_DELIMITED_JSON', 'AVRO'
+//        BigQueryFileFormat.fromName( conf.get( OUTPUT_BQFF, "CSV" ) ),
+//        (Class<? extends FileOutputFormat>) conf.getJobConf().getOutputFormat().getClass() );
+//
+//    } catch ( IOException e ) {
+//      throw new ShimRuntimeException( "Unable to create configuration for new dataproc api: ", e );
+//    }
+
+
+    return conf;
+  }
+
+}
